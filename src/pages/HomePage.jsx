440059b72ced77a09e762b42e54ca7b3c7431fb0
Daniel-Di-Fraia <daniel.difraia2600@gmail.com>
2026-02-11 12:11:10 +0100

    inizio base homepage pagina + css

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,22 @@
+import '../pages css/HomePage.css'
+
 function HomePage() {
-  
+
 
   return (
     <>
-      <h1>Homepage</h1>
+      <main className="home">
+        <section className="container">
+        <h1>Benvenuto nel tuo catalogo giochi!</h1>
+        <p>
+          Scorri, confronta e trova il gioco perfetto per il tuo mood del momento.
+          Aggiungi ai preferiti i titoli top, crea una lista personalizzata da consultare quando vuoi e preparati a partire: la prossima avventura ti aspetta.
+        </p>
+        </section>
+        {/* <section className="container">
+          <h2>Generi in Tendenza</h2>
+        </section> */}
+      </main>
     </>
   )
 }
